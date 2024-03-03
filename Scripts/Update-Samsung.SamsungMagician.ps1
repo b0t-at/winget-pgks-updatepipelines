0cd2c6502f1d19941aa8002efb22477069a6a959
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-03-03 19:41:48 +0100

    Add Samsung.SamsungMagician (#5)
    * Add repository sync step to workflow
    
    * Update workflow and scripts for qFlipper package
    
    * Update qFlipper workflow and script
    
    * fix PR name
    
    * Update qFlipper workflow and add script to update Samsung Magician
    
    * Add Samsung.SamsungMagician workflow and update qFlipper workflow

---
+++ Scripts/Update-Samsung.SamsungMagician.ps1
@@ -0,0 +1,58 @@
+if ($Env:GITHUB_TOKEN) {
+    Write-Host 'GITHUB_TOKEN detected'
+    $gitToken = ${Env:GITHUB_TOKEN}
+}
+else {
+    Write-Host 'GITHUB_TOKEN not detected'
+    exit 1
+}
+
+$wingetPackage = "Samsung.SamsungMagician"
+
+# scrap website for download link https://semiconductor.samsung.com/consumer-storage/support/tools/
+$website = "https://www.samsung.com/semiconductor/minisite/ssd/download/tools/"
+$websiteContent = Invoke-WebRequest -Uri $website
+
+$links = $websiteContent.Links | Where-Object { $_.href -like "*.exe" -and $_.href -like "*Samsung_Magician_Installer*" }
+
+$latestVersionUrl = $links[0].href
+
+# get full name of file from link
+$fileName = $latestVersionUrl.Split("/")[-1]
+
+#download exe File
+Invoke-WebRequest -Uri $latestVersionUrl -OutFile $fileName
+
+$file = Get-ChildItem -Path $fileName
+$latestVersion = $file.VersionInfo.ProductVersion
+
+
+
+$prMessage = "Update version: $wingetPackage version $latestVersion"
+
+$foundMessage, $textVersion, $separator, $wingetVersions = winget search --id $wingetPackage --source winget --versions
+
+# Check for existing versions in winget
+if ($wingetVersions -contains $latestVersion) {
+    Write-Output "Latest version of $wingetPackage $latestVersion is already present in winget."
+}
+else {
+    # Check for existing PRs
+    $ExistingPRs = gh pr list --search "$($wingetPackage) version $($latestVersion) in:title draft:false" --state 'all' --json 'title,url' --repo 'microsoft/winget-pkgs' | ConvertFrom-Json
+
+    if ($ExistingPRs.Count -gt 0) {
+        Write-Output "$foundMessage"
+        $ExistingPRs | ForEach-Object {
+            Write-Output "Found existing PR: $($_.title)"
+            Write-Output "-> $($_.url)"
+        }
+    }
+    elseif ($wingetVersions -and ($wingetVersions -notmatch $latestVersion)) {
+        gh repo sync $Env:WINGET_PKGS_FORK_REPO -b main
+        .\wingetcreate.exe update $wingetPackage -s -v $ver -u "$latestVersionUrl" --prtitle $prMessage -t $gitToken
+    }
+    else { 
+        Write-Output "$foundMessage"
+        Write-Output "No existing PRs found. Check why wingetcreate has not run."
+    }
+}
