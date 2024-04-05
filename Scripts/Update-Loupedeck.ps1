27fe725237b4b68a79a7dfda96187241856ecab2
Alexander Mnich <alex@mnich.dev>
2024-04-05 17:53:24 +0200

    fix loupedeck download

--- Scripts/Update-Loupedeck.ps1
+++ Scripts/Update-Loupedeck.ps1
@@ -32,7 +32,9 @@ if ($wingetVersions.Contains($versionInfo)) {
     exit 0
 }
 
-$fullDownloadURL = "https://download.loupedeck.com/software/latest/LoupedeckInstaller_" + $versionInfo + ".exe"
+# extract major and minor version e.g. 5.9 from 5.9.10
+$majorMinorVersion = $versionInfo -replace '\.\d+.\d+$'
+$fullDownloadURL = "https://5145542.fs1.hubspotusercontent-na1.net/hubfs/5145542/Knowledge%20Base/LD%20Software%20Downloads/$majorMinorVersion/LoupedeckInstaller_" + $versionInfo + ".exe"
 
 # check if full download URL is valid
 $fullDownloadURLResponse = Invoke-WebRequest -Uri $fullDownloadURL -UseBasicParsing -Method Head
