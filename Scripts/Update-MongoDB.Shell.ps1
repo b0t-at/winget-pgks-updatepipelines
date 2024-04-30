5eb26ebeb4e350074a34dde0f8917852221ff532
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-04-30 10:04:39 +0000

    Update MongoDB.Server workflow and script

--- Scripts/Update-MongoDB.Shell.ps1
+++ Scripts/Update-MongoDB.Shell.ps1
@@ -19,17 +19,17 @@ $website = Invoke-WebRequest -Uri $url
 $content = $website.Content
 
 # Find all strings that look like links and end with .msi
-$links = $content | Select-String -Pattern 'https?://[^\s]*\.msi' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
-
+$links = $content | Select-String -Pattern 'https?://[^"]+' -AllMatches | % { $_.Matches } | % { $_.Value }
+$mongoshlinks = $links | Select-String -Pattern 'https?://[^\s]*mongosh[^\s]*\.msi' -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
 # Extract versions from the links
-$versions = $links | ForEach-Object { $_ -match '(\d+\.\d+\.\d+(-rc\d*)?)' | Out-Null; $matches[1] }
+$versions = $mongoshlinks | ForEach-Object { $_ -match '(\d+\.\d+\.\d+(-rc\d*)?)' | Out-Null; $matches[1] }
 
 # Exclude release candidates
 $stableVersions = $versions | Where-Object { $_ -notmatch '-rc' }
 
 # Sort the versions and get the latest one
 $latestVersion = $stableVersions | Sort-Object {[Version]$_} | Select-Object -Last 1
-$latestVersionUrl = $links | Where-Object { $_ -match $latestVersion }
+$latestVersionUrl = $mongoshlinks | Where-Object { $_ -match $latestVersion }
 
 
 Write-Host "Version found: $latestVersion"
