04d183548241ab9410608b282b7d7eeeea1d3656
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-10 21:58:18 +0000

    ```text refactor: Update SmartProjects.IsoBuster package in update-via-script.yml
    This commit adds the SmartProjects.IsoBuster package to the update-via-script.yml file. The package URL has been updated to include two different versions of the installer. This change allows for the download of the appropriate version of the package.

---
+++ Scripts/Update-SmartProjects.IsoBuster.ps1
@@ -0,0 +1,87 @@
+if ($Env:GITHUB_TOKEN) {
+    Write-Host 'GITHUB_TOKEN detected'
+    $gitToken = ${Env:GITHUB_TOKEN}
+}
+else {
+    Write-Host 'GITHUB_TOKEN not detected'
+    exit 1
+}
+
+$wingetPackage = "Logitech.OptionsPlus"
+$url = ${Env:WebsiteURL}
+
+# download latest version from loupedeck.com and get version by filename
+$latestVersionUrl = $url -split " " | Select-Object -First 1
+#create directory downloads and change into it
+$DownloadFileName = "IsoBuster_installer.exe"
+Invoke-WebRequest -Uri $latestVersionUrl -OutFile $DownloadFileName
+$file = Get-ChildItem -Path $DownloadFileName
+$versionInfo = $file.VersionInfo.ProductVersion
+
+if ($null -eq $versionInfo) {
+    Write-Host "Could not find version info in file"
+    exit 1
+}
+
+Write-Host "Found latest version: $versionInfo"
+
+$latestVersion = $versionInfo
+
+
+#$prMessage = "Update version: $wingetPackage version $latestVersion"
+
+$ghVersionURL = "https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/$($wingetPackage.Substring(0, 1).ToLower())/$($wingetPackage.replace(".","/"))/$latestVersion/$wingetPackage.yaml"
+$ghCheckURL = "https://github.com/microsoft/winget-pkgs/blob/master/manifests/$($wingetPackage.Substring(0, 1).ToLower())/$($wingetPackage.replace(".","/"))/"
+
+
+# Check if package is already in winget
+$ghCheck = Invoke-WebRequest -Uri $ghCheckURL -Method Head -SkipHttpErrorCheck 
+if ($ghCheck.StatusCode -eq 404) {
+    Write-Output "Packet not yet in winget. Please add new Packet manually"
+    exit 1
+} 
+
+$ghVersionCheck = Invoke-WebRequest -Uri $ghVersionURL -Method Head -SkipHttpErrorCheck  
+
+#$foundMessage, $textVersion, $separator, $wingetVersions = winget search --id $wingetPackage --source winget --versions
+
+if ($ghVersionCheck.StatusCode -eq 200) {
+    Write-Output "Latest version of $wingetPackage $latestVersion is already present in winget."
+    exit 0
+}
+else {
+
+
+
+    # Check for existing PRs
+    $ExistingOpenPRs = gh pr list --search "$($wingetPackage) $($latestVersion) in:title draft:false" --state 'open' --json 'title,url' --repo 'microsoft/winget-pkgs' | ConvertFrom-Json
+    $ExistingMergedPRs = gh pr list --search "$($wingetPackage) $($latestVersion) in:title draft:false" --state 'merged' --json 'title,url' --repo 'microsoft/winget-pkgs' | ConvertFrom-Json
+
+    $ExistingPRs = @($ExistingOpenPRs) + @($ExistingMergedPRs)    
+    # TODO Check if PR is already merged, if so exit
+
+    # TODO if PR from us is already open, update PR with new version
+
+    # TODO if PR is closed, not from us and no PR got merged, create new PR
+
+    # TODO if PR is closed, from us and no PR got merged, throw error
+
+    if ($ExistingPRs.Count -gt 0) {
+        Write-Output "$foundMessage"
+        $ExistingPRs | ForEach-Object {
+            Write-Output "Found existing PR: $($_.title)"
+            Write-Output "-> $($_.url)"
+        }
+    }
+    elseif ($ghCheck.StatusCode -eq 200) {
+        Write-Output "Downloading wingetcreate and open PR for $wingetPackage Version $latestVersion"
+#        Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
+#        .\wingetcreate.exe update $wingetPackage -s -v $latestVersion -u "$latestVersionUrl" --prtitle $prMessage -t $gitToken
+        Invoke-WebRequest "https://github.com/russellbanks/Komac/releases/download/v2.2.1/KomacPortable-x64.exe" -OutFile komac.exe
+        .\komac.exe update --identifier $wingetPackage --version $latestVersion --urls $url -s -t $gitToken
+    }
+    else { 
+        Write-Output "$foundMessage"
+        Write-Output "No existing PRs found. Check why wingetcreate has not run."
+    }
+}
