cb9ac51f6c2bc665b5f080d7c284044d068ca3d5
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-10 22:02:50 +0000

    refactor: Trim product version in Update-SmartProjects.IsoBuster.ps1
    The `Update-SmartProjects.IsoBuster.ps1` script has been modified to trim the product version retrieved from the file. This change ensures that any leading or trailing whitespace is removed from the version information.

--- Scripts/Update-SmartProjects.IsoBuster.ps1
+++ Scripts/Update-SmartProjects.IsoBuster.ps1
@@ -16,7 +16,7 @@ $latestVersionUrl = $url -split " " | Select-Object -First 1
 $DownloadFileName = "IsoBuster_installer.exe"
 Invoke-WebRequest -Uri $latestVersionUrl -OutFile $DownloadFileName
 $file = Get-ChildItem -Path $DownloadFileName
-$versionInfo = $file.VersionInfo.ProductVersion
+$versionInfo = $file.VersionInfo.ProductVersion.trim()
 
 if ($null -eq $versionInfo) {
     Write-Host "Could not find version info in file"
