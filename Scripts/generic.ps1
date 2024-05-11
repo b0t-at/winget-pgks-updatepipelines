4f8d50c9fbc49e367b2b752a509454f5b7c0d847
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-11 16:38:40 +0000

    Refactor Update-WingetPackage function to use consistent capitalization for the 'With' parameter

--- Scripts/generic.ps1
+++ Scripts/generic.ps1
@@ -2,5 +2,11 @@
 
 #### Main
 
-Update-WingetPackage -wingetPackage ${Env:PackageName} -With ${Env:With} -WebsiteURL ${Env:WebsiteURL}
+if (${Env:With}) {
+    Update-WingetPackage -wingetPackage ${Env:PackageName} -With ${Env:With} -WebsiteURL ${Env:WebsiteURL}
+}
+else {
+    Update-WingetPackage -wingetPackage ${Env:PackageName} -WebsiteURL ${Env:WebsiteURL}
+}
+
 
