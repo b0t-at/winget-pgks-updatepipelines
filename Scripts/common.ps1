54cac928218ad96c793fc74c20960613849c4813
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-16 18:56:51 +0000

    Refactor Update-WingetPackage function to improve URL handling and handle optional resolves parameter

--- Scripts/common.ps1
+++ Scripts/common.ps1
@@ -269,7 +269,7 @@ function Update-WingetPackage {
             Switch ($With) {
                 "Komac" {
                     Install-Komac
-                    .\komac.exe update --identifier $wingetPackage --version $Latest.Version --urls $Latest.URLs ($Submit -eq $true ? "-s" : "-dry_run") ($resolves -match '^\d+$' ? "-resolves $resolves": "") -t $gitToken -output "$ManifestOutPath"
+                    .\komac.exe update --identifier $wingetPackage --version $Latest.Version --urls $Latest.URLs ($Submit -eq $true ? '-s' : '-dry_run') ($resolves -match '^\d+$' ? ('-resolves '+$resolves) : "") -t $gitToken -output "$ManifestOutPath"
                 }
                 "WinGetCreate" {
                     Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
