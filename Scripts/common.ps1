9405f2f6425b680e3aed8b44eed2f81e6c38f22a
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-16 19:02:33 +0000

    Refactor Update-WingetPackage function to improve URL handling and handle optional resolves parameter

--- Scripts/common.ps1
+++ Scripts/common.ps1
@@ -229,6 +229,10 @@ function Update-WingetPackage {
         throw "Either WebsiteURL or both latestVersion and latestVersionURL are required."
     }
 
+    if ($Submit -eq $false) {
+        $env:DRY_RUN = $true
+    }
+
 
     $gitToken = Test-GitHubToken
 
@@ -257,7 +261,9 @@ function Update-WingetPackage {
     $ManifestOutPath = "./manifests/$($wingetPackage.Substring(0, 1).ToLower())/$($wingetPackage.replace(".","/"))/$latestVersion/"
 
     write-host "($resolves -match '^\d+$' ? "-resolves $resolves": "")"
-    write-host "($Submit -eq $true ? "-s" : "-dry_run")"
+    write-host ($resolves -match '^\d+$' ? "-resolves $resolves": "")
+    write-host "($resolves -match '^\d+$' ? ('-resolves '+$resolves) : "")"
+    write-host ($resolves -match '^\d+$' ? ('-resolves '+$resolves) : "")
     
 
     if ($PackageAndVersionInWinget) {
@@ -269,7 +275,7 @@ function Update-WingetPackage {
             Switch ($With) {
                 "Komac" {
                     Install-Komac
-                    .\komac.exe update --identifier $wingetPackage --version $Latest.Version --urls $Latest.URLs ($Submit -eq $true ? '-s' : '-dry_run') ($resolves -match '^\d+$' ? ('-resolves '+$resolves) : "") -t $gitToken -output "$ManifestOutPath"
+                    .\komac.exe update --identifier $wingetPackage --version $Latest.Version --urls $Latest.URLs ($Submit -eq $true ? '-s' : '') ($resolves -match '^\d+$' ? ('-resolves '+$resolves) : "") -t $gitToken -output "$ManifestOutPath"
                 }
                 "WinGetCreate" {
                     Invoke-WebRequest https://aka.ms/wingetcreate/latest -OutFile wingetcreate.exe
