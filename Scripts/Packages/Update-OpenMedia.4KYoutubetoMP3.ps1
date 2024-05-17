1c6d0acabb4bcbbef20e51856b0438a83558a159
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-17 13:41:48 +0000

    feat: Update OpenMedia.4KYoutubetoMP3 script to retrieve latest version and URL

--- Scripts/Packages/Update-OpenMedia.4KYoutubetoMP3.ps1
+++ Scripts/Packages/Update-OpenMedia.4KYoutubetoMP3.ps1
@@ -16,14 +16,18 @@ $WebsiteLinks = $website.Links
 
 $FilteredLinks = $WebsiteLinks | Where-Object { $_.Id -match $URLFilter }
 
+Write-Host "Filtered Links: $FilteredLinks"
+
 $versions = $FilteredLinks.outerHTML | Select-String -Pattern $versionPattern -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
 
 
 $latestVersionUrl = $FilteredLinks | ForEach-Object { ($_.href -replace '\?.*', '') }
 
+Write-Host "latestVersionUrl: $latestVersionUrl"
+
 $64bitCheckURL = $($latestVersionUrl| Where-Object { $_ -match "x64_online.exe" })
 
-Write-Host "Checking $64bitCheckURL"
+Write-Host "Checking: $64bitCheckURL"
 
 $latestVersion = Get-ProductVersionFromFile -VersionInfoProperty "ProductVersion" -WebsiteURL $64bitCheckURL
 
