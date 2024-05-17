a2181396bb5703a30039f6d1ed5e90829913fe6b
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-17 13:05:02 +0000

    add 4kyoutubetomp3

---
+++ Scripts/Packages/Update-OpenMedia.4KYoutubetoMP3.ps1
@@ -0,0 +1,25 @@
+. .\Scripts\common.ps1
+
+$versionParts = $wingetPackage.Split('.')
+$PackageName = $versionParts[1]
+
+$ProductName = ($PackageName -replace '4K', '').Trim().ToLower()
+
+$versionPattern = "$($ProductName)_(\d+\.\d+\.\d+\.\d+)"
+$URLFilter = "$($ProductName)_windows_(x32|x64)"
+
+# Download the webpage
+$website = Invoke-WebRequest -Uri $WebsiteURL
+
+# Extract the content of the webpage
+$WebsiteLinks = $website.Links
+
+$FilteredLinks = $WebsiteLinks | Where-Object { $_.Id -match $URLFilter }
+
+$versions = $FilteredLinks.outerHTML | Select-String -Pattern $versionPattern -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
+
+$latestVersion = $versions | Sort-Object -Descending -Unique | Select-Object -First 1
+
+$latestVersionUrl = $FilteredLinks | ForEach-Object { ($_.href -replace '\?.*', '') }
+
+return $latestVersion, $latestVersionUrl
