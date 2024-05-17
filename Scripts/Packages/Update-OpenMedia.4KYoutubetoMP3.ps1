607eb05ab655cba43b681504e05c1ab2d152a7ac
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2024-05-17 13:48:15 +0000

    feat: Update OpenMedia.4KYoutubetoMP3 script to improve URL filtering

--- Scripts/Packages/Update-OpenMedia.4KYoutubetoMP3.ps1
+++ Scripts/Packages/Update-OpenMedia.4KYoutubetoMP3.ps1
@@ -16,7 +16,7 @@ $WebsiteLinks = $website.Links
 
 $FilteredLinks = $WebsiteLinks | Where-Object { $_.Id -match $URLFilter }
 
-Write-Host "Filtered Links: $FilteredLinks"
+Write-Host "Filtered Links: $($FilteredLinks.href)"
 
 $versions = $FilteredLinks.outerHTML | Select-String -Pattern $versionPattern -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
 
@@ -25,7 +25,7 @@ $latestVersionUrl = $FilteredLinks | ForEach-Object { ($_.href -replace '\?.*', 
 
 Write-Host "latestVersionUrl: $latestVersionUrl"
 
-$64bitCheckURL = $($latestVersionUrl| Where-Object { $_ -match "x64_online.exe" })
+$64bitCheckURL = $($latestVersionUrl| Where-Object { $_ -match "_online.exe" })
 
 Write-Host "Checking: $64bitCheckURL"
 
