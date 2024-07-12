e3de13d32b4ee86fbeed01fa102c0fa9dfb68b3e
Alexander Mnich <alex@mnich.dev>
2024-07-12 22:58:33 +0200

    fix komac url

--- Scripts/common.ps1
+++ Scripts/common.ps1
@@ -177,7 +177,7 @@ function Get-ProductVersionFromFile {
 
 function Install-Komac {
     if (-not (Test-Path ".\komac.exe")) {
-        $latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/latest").assets | Where-Object { $_.browser_download_url.EndsWith("komac-setup-2.3.0-x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
+        $latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/latest").assets | Where-Object { $_.browser_download_url.EndsWith("komac-2.3.0-x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
         #$latestKomacRelease = "https://github.com/b0t-at/Komac/releases/download/v2.99/KomacPortable-x64.exe"
         Invoke-WebRequest  -Uri $latestKomacRelease -OutFile komac.exe
     }
