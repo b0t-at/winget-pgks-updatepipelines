c6eadef175eb52a708af9bd6affacb053b0f5db0
Alexander Mnich <alex@mnich.dev>
2024-07-12 22:42:02 +0200

    add Microsoft.AzureMonitorAgent

---
+++ Scripts/Packages/Update-Microsoft.AzureMonitorAgent.ps1
@@ -0,0 +1,16 @@
+. .\Scripts\common.ps1
+
+$website = $WebsiteURL
+$absoluteURL=[System.Net.HttpWebRequest]::Create($website).GetResponse().ResponseUri.AbsoluteUri
+$versionWebsite = Invoke-WebRequest -Method Get "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/azure-monitor/agents/azure-monitor-agent-extension-versions.md"
+$lines = $versionWebsite.Content -split "`n"
+
+# Find the index of the table header separator
+$schema = "| Release Date | Release notes | Windows | Linux |"
+$index = $lines.IndexOf($schema)
+# get column index of Windows in schema
+$columnIndex = $schema.Split("|").IndexOf(" Windows ")
+# Find the index of the table header separator
+$versionInfo = $lines[$index + 2].Split("|")[$columnIndex].Trim()
+
+return $versionInfo, $absoluteURL
