56bab137aff286ffd979889b9568a9e154c10df0
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2024-08-31 23:13:11 +0200

    update package removal script

--- Scripts/Remove-BrokenPackages.ps1
+++ Scripts/Remove-BrokenPackages.ps1
@@ -1,12 +1,20 @@
 $packages = Import-Csv -Path .\broken_package.csv
+$DRY_RUN = $false
+
 $successfullPackages = @()
 foreach ($package in $packages) {
     $prBody = "installers return following HTTP status code: $($package.http_codes)"
-    . winget.exe download --id $package.package_identifier --version $package.package_version
+    $output = & winget.exe download --id $package.package_identifier --version $package.package_version
 
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "Package $($package.package_identifier) | $($package.package_version) | Failed to download"
-        . komac.exe remove --reason $prBody --submit -v $package.package_version  $package.package_identifier 
+        if([string]::Join('`n',$output).Contains($package.http_codes)) {
+            Write-Host "Package $($package.package_identifier) | $($package.package_version) | Failed to download"
+            if($DRY_RUN -eq $false) {
+            . komac.exe remove --reason $prBody --submit -v $package.package_version  $package.package_identifier        
+            }
+        } else {
+            Write-Warning "HTTP status code mismatch for package $($package.package_identifier) | $($package.package_version)"
+        }
         continue
     }
 
