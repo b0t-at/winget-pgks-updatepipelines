9c7f47ad4590832f556cc8b620c5df0872fac80d
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2024-09-11 10:44:43 +0000

    create Betterbird.Betterbird

---
+++ Scripts/Packages/Update-Betterbird.Betterbird.ps1
@@ -0,0 +1,22 @@
+. .\Scripts\common.ps1
+
+$versionPattern = "<h3>Betterbird (\d+\.\d+\.\d+-bb\d+)"
+
+$URLFilter = "(.exe|.zip)"
+
+# Download the webpage
+$website = Invoke-WebRequest -Uri $WebsiteURL
+
+# Extract the content of the webpage
+$WebsiteLinks = $website.Links
+$WebsiteContent = $website.Content
+
+$FilteredLinks = $WebsiteLinks | Where-Object { $_.Download -match $URLFilter }
+
+$versions = $WebsiteContent | Select-String -Pattern $versionPattern -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Groups[1].Value }
+
+$latestVersion = $versions | Sort-Object -Descending -Unique | Select-Object -First 1
+
+$latestVersionUrl = $FilteredLinks | ForEach-Object { ($_.href -replace '\?.*', '') } | Where-Object { $_ -ne '' -and $_ -match $latestVersion -and $_ -notmatch "latest-build|Previous" } | ForEach-Object { $WebsiteURL + $_ }
+
+return $latestVersion, $latestVersionUrl
