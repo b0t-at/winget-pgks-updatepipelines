44fe4e7d3b6022c0186c7bbb50286bac3642f9b1
Alexander Mnich <alex@mnich.dev>
2024-10-03 15:37:59 +0200

    add Bandisoft.Bandizip

---
+++ Scripts/Packages/Update-Bandisoft.Bandizip.ps1
@@ -0,0 +1,10 @@
+. .\Scripts\common.ps1
+
+$latestVersion = Get-ProductVersionFromFile -WebsiteURL "https://dl.bandisoft.com/bandizip.std/BANDIZIP-SETUP-STD-X64.EXE" -VersionInfoProperty ProductVersion
+
+$returnObject = [PSCustomObject]@{
+    Version = $latestVersion
+    URLs = $latestVersionUrl
+}
+
+return $returnObject
