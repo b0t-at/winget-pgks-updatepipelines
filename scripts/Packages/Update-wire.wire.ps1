0a1f9c88c12b68456f22217ab58c09228eaf4b68
Alexander Mnich <alex@mnich.dev>
2024-11-06 22:16:56 +0100

    add wire.wire

---
+++ scripts/Packages/Update-wire.wire.ps1
@@ -0,0 +1,23 @@
+
+$repo = "wireapp/wire-desktop"
+# get releases from github with github cli
+$releases = gh release list -R $repo --json name,isLatest,tagName --order desc | ConvertFrom-Json
+
+# get latest release where tag starts with windows
+$latestWindowsTag = $null
+foreach($release in $releases) {
+    if($release.tagName -like "windows*") {
+        $latestWindowsTag = $release.tagName
+        break
+    }
+}
+
+$latestVersion = $latestWindowsTag.Split("/")[1]
+$latestVersionUrl = "https://github.com/wireapp/wire-desktop/releases/download/windows%2F$version/Wire-Setup.exe"
+
+$returnObject = [PSCustomObject]@{
+    Version = $latestVersion
+    URLs = $latestVersionUrl
+}
+
+return $returnObject
