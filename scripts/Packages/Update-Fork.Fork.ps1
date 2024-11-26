5ff047fefda55aa7f008ce11b613f8b0e772fc10
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2024-11-26 13:17:56 +0100

    fix fork.fork

--- scripts/Packages/Update-Fork.Fork.ps1
+++ scripts/Packages/Update-Fork.Fork.ps1
@@ -16,7 +16,7 @@
 # $latestVersionUrl = "https://cdn.fork.dev/win/Fork-$latestVersion.exe"
 
 $websiteContent = Invoke-WebRequest -Uri $WebsiteURL
-$latestVersionUrl = $websiteContent.Links | Where-Object {$_.tagName -eq "A" -and $_.outerHTML.Contains('"downloadBtn2Win"')} | Select-Object -First 1 -ExpandProperty href
+$latestVersionUrl = $websiteContent.Links | Where-Object {$_.tagName -eq "A" -and ($_.outerHTML.Contains('"downloadBtn2Win"') -or $_.outerHTML.Contains('"downloadBtn1Win"'))} | Select-Object -First 1 -ExpandProperty href
 # extract version from link
 $latestVersion = [regex]::Match($latestVersionUrl, '.*Fork-(.*).exe').Groups[1].Value
 
