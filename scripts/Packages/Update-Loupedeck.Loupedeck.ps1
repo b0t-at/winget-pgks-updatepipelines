bba10ebab6d0745704a35d832c76b55dead1c972
Alexander Mnich <alex@mnich.dev>
2024-12-04 10:40:36 +0100

    fix Loupedeck.Loupedeck

--- scripts/Packages/Update-Loupedeck.Loupedeck.ps1
+++ scripts/Packages/Update-Loupedeck.Loupedeck.ps1
@@ -1,23 +1,27 @@
+#$WebsiteURL = "https://download.loupedeck.com/software/latest-win"
+$WebsiteURL = "https://support.loupedeck.com/loupedeck-software-download"
 
+$websiteData = Invoke-WebRequest -Method Get -Uri $WebsiteURL
+
+$installerLink = ($websiteData.Links | Where-Object { $_.href -like "*.exe" } | Select-Object -ExpandProperty href).ToString()
+
+if($installerLinks.Count -eq 0 -or $installerLinks.Count -gt 1) {
+    Write-Host "No installer links or too much installer links found"
+    exit 1
+}
+
+# get substring position "SoftwareDownloads and replace everything until there with prefix url"
+$installerLink = $installerLink.Substring($installerLink.IndexOf("LD%20Software%20Downloads"))
+$fullDownloadURL = "https://support.loupedeck.com/hubfs/Knowledge%20Base/$installerLink"
+
+# check if full download URL is valid
+Write-Host "Full download URL: $fullDownloadURL"
 
 # download latest version from loupedeck.com and get version by filename
-$versionInfo = Get-ProductVersionFromFile -WebsiteURL $WebsiteURL -VersionInfoProperty "ProductVersion"
-
+$versionInfo = Get-ProductVersionFromFile -WebsiteURL $fullDownloadURL -VersionInfoProperty "ProductVersion"
 
 Write-Host "Found latest version: $versionInfo"
 $latestversion = $versionInfo
-# extract major and minor version e.g. 5.9 from 5.9.10
-$majorMinorVersion = $versionInfo -replace '\.\d+$'
-$fullDownloadURL = "https://support.loupedeck.com/hubfs/Knowledge%20Base/LD%20Software%20Downloads/$majorMinorVersion/LoupedeckInstaller_" + $versionInfo + ".exe"
-# check if full download URL is valid
-Write-Host "Full download URL: $fullDownloadURL"
-$fullDownloadURLResponse = Invoke-WebRequest -Uri $fullDownloadURL -UseBasicParsing -Method Head -ErrorAction Continue
-if ($fullDownloadURLResponse.StatusCode -ne 200) {
-    Write-Host "Current download URL is not valid. Status Code: $($fullDownloadURLResponse.StatusCode) Checking alternative URL"
-    $majorMinorVersion = $versionInfo -replace '\.\d+\.\d+$'
-    $fullDownloadURL = "https://support.loupedeck.com/hubfs/Knowledge%20Base/LD%20Software%20Downloads/$majorMinorVersion/LoupedeckInstaller_" + $versionInfo + ".exe"
-    Write-Host "Alternative download URL: $fullDownloadURL"
-}
 
 # check if full download URL is valid
 $fullDownloadURLResponse = Invoke-WebRequest -Uri $fullDownloadURL -UseBasicParsing -Method Head
