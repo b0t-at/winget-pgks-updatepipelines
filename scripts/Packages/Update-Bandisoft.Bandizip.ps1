67786af444c90955267c2be0a77c5b5577b7852a
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2025-01-23 17:13:13 +0000

    fix URL Bandizip

--- scripts/Packages/Update-Bandisoft.Bandizip.ps1
+++ scripts/Packages/Update-Bandisoft.Bandizip.ps1
@@ -1,4 +1,5 @@
-$latestVersion = Get-ProductVersionFromFile -WebsiteURL "https://dl.bandisoft.com/bandizip.std/BANDIZIP-SETUP-STD-X64.EXE" -VersionInfoProperty ProductVersion
+$latestVersionUrl = "https://dl.bandisoft.com/bandizip.std/BANDIZIP-SETUP-STD-X64.EXE"
+$latestVersion = Get-ProductVersionFromFile -WebsiteURL $latestVersionUrl  -VersionInfoProperty ProductVersion
 
 return [PSCustomObject]@{
     Version = $latestVersion
