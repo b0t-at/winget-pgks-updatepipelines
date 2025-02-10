d1a15e16a750d1169f06b523df88e3146530b346
Alexander Mnich <alex@mnich.dev>
2025-02-10 17:05:14 +0100

    rewrite Fork.Fork to release index

--- scripts/Packages/Update-Fork.Fork.ps1
+++ scripts/Packages/Update-Fork.Fork.ps1
@@ -1,9 +1,40 @@
-$websiteContent = Invoke-WebRequest -Uri $WebsiteURL
-$latestVersionUrl = $websiteContent.Links | Where-Object {$_.tagName -eq "A" -and ($_.outerHTML.Contains('"downloadBtn2Win"') -or $_.outerHTML.Contains('"downloadBtn1Win"'))} | Select-Object -First 1 -ExpandProperty href
+#$websiteContent = Invoke-WebRequest -Uri $WebsiteURL
+#$latestVersionUrl = $websiteContent.Links | Where-Object {$_.tagName -eq "A" -and ($_.outerHTML.Contains('"downloadBtn2Win"') -or $_.outerHTML.Contains('"downloadBtn1Win"'))} | Select-Object -First 1 -ExpandProperty href
 # extract version from link
-$latestVersion = [regex]::Match($latestVersionUrl, '.*Fork-(.*).exe').Groups[1].Value
+#$latestVersion = [regex]::Match($latestVersionUrl, '.*Fork-(.*).exe').Groups[1].Value
+
+$releasesURL = 'https://git-fork.com/update/win/RELEASES'
+$versionRegex = "([A-Z,0-9]{0,128}) Fork-(\d\.\d{1,2}\.\d{1,2})(?:-full.nupkg \d.+)"
+
+$releasesRaw = Invoke-WebRequest -Uri $releasesURL
+$releasesDecoded = [System.Text.Encoding]::ASCII.GetString($releasesRaw.Content)
+$releaseValues = ($releasesDecoded | select-string -pattern $versionRegex -AllMatches).Matches
+
+$releaseFound = $false
+# reverse releaseValues list
+$releaseValues = $releaseValues | ForEach-Object { $_ } | Sort-Object -Property Index -Descending
+
+foreach ($release in $releaseValues) {
+    $latestVersion = $release.Groups[2].Value
+    $latestVersionUrl = "https://cdn.fork.dev/win/Fork-" + $latestVersion + ".exe"
+    try {
+        $res = Invoke-WebRequest -Method Head -Uri $latestVersionUrl
+    }
+    catch {
+        Write-Host "Failed to get $latestVersionUrl"
+        continue
+    }
+    if ($res.StatusCode -eq 200) {
+        $releaseFound = $true
+        break
+    }
+}
+
+if (-not $releaseFound) {
+    throw "No release found"
+}
 
 return [PSCustomObject]@{
     Version = $latestVersion
-    URLs = $latestVersionUrl
+    URLs    = $latestVersionUrl
 }
