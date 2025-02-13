463d880d805483486230e26247738a74fd42eadb
Alexander Mnich <56564725+a-mnich@users.noreply.github.com>
2025-02-13 12:00:41 +0100

    fix komac nightly install

--- modules/WingetMaintainerModule/Public/Install-Komac.ps1
+++ modules/WingetMaintainerModule/Public/Install-Komac.ps1
@@ -2,7 +2,7 @@ function Install-Komac {
     if (-not (Test-Path ".\komac.exe")) {
         #$latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/latest").assets | Where-Object { $_.browser_download_url.EndsWith("x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
         #nightly
-        $latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/nightly").assets | Where-Object { $_.browser_download_url.EndsWith("komac-nightly-x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
+        $latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/tags/nightly").assets | Where-Object { $_.browser_download_url.EndsWith("komac-nightly-x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
         Invoke-WebRequest  -Uri $latestKomacRelease -OutFile komac.exe
     }
 
