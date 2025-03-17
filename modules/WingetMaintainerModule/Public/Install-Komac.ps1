bfff01f32e530a5668e1d52ab2517f0442ef85bb
Alexander Mnich <alex@mnich.dev>
2025-03-17 18:15:23 +0100

    check for installed komac

--- modules/WingetMaintainerModule/Public/Install-Komac.ps1
+++ modules/WingetMaintainerModule/Public/Install-Komac.ps1
@@ -1,4 +1,11 @@
 function Install-Komac {
+    $executable = Get-Command "komac.exe" -ErrorAction SilentlyContinue
+    if ($null -ne $executable) {
+        Write-Host "Komac is already installed"
+        return
+    }
+
+
     if (-not (Test-Path ".\komac.exe")) {
         #latest
         $latestKomacRelease = (Invoke-RestMethod -Uri "https://api.github.com/repos/russellbanks/Komac/releases/latest").assets | Where-Object { $_.browser_download_url.EndsWith("x86_64-pc-windows-msvc.exe") } | Select-Object -First 1 -ExpandProperty browser_download_url
