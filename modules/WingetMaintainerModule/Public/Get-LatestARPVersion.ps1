6fbd616e494b9c2fa266d9a4f9f2cf0c9779d8e1
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2025-05-03 14:27:26 +0200

    fix add github

--- modules/WingetMaintainerModule/Public/Get-LatestARPVersion.ps1
+++ modules/WingetMaintainerModule/Public/Get-LatestARPVersion.ps1
@@ -6,18 +6,20 @@ function Get-LatestARPVersion {
         [Parameter(Mandatory = $false)][object]$ListToTrimFromTag = @("v", "V", "RELEASE_"),
         [Parameter(Mandatory = $true)][string]$GHURLs
     )
-
+    $splittedGHURLs = $GHURLs.split(" ")
     # If the GHURLs contain {ARPVERSION}, find the ARP version from the asset links via regex
-    if ($GHURLs -match "{ARPVERSION}") {
+    if ( $splittedGHURLs -match "{ARPVERSION}") {
         $latestRelease = gh release view --repo $Repo $Tag --json assets | ConvertFrom-Json
 
         # Build the regex pattern from GHURLs, capturing the ARP version
-        $regex = $GHURLs -replace "{ARPVERSION}", "(.+?)" -replace "{TAG}", [Regex]::Escape($Tag)
+        $regexURLS = $splittedGHURLs -replace "{ARPVERSION}", "(.+?)" -replace "{TAG}", [Regex]::Escape($Tag)
         
         # Loop through assets and return first captured ARP version from the URL
         foreach ($asset in $latestRelease.assets) {
-            if ($asset.url -match $regex) {
-                return $matches[1]
+            foreach ($regex in $regexURLS) {
+                if ($asset.url -match $regex) {
+                    return $matches[1]
+                }
             }
         }
         throw "No ARP version found in the assets for tag $Tag in repo $Repo"
